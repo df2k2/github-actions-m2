73e7aecf687e42003075d6f525d00a34c3046bfc
Chris Snedaker <df2002@gmail.com>
2022-02-17 21:05:06 -0500

    Update entrypoint.sh

--- magento-coding-standard/entrypoint.sh
+++ magento-coding-standard/entrypoint.sh
@@ -8,7 +8,7 @@ echo "::add-matcher::${HOME}/problem-matcher.json"
 cd $GITHUB_WORKSPACE
 
 test -z "${INPUT_PHPCS_STANDARD}" && INPUT_PHPCS_STANDARD=Magento2
-test -z "${INPUT_PHPCS_SEVERITY}" && INPUT_PHPCS_SEVERITY=8
+test -z "${INPUT_PHPCS_SEVERITY}" && INPUT_PHPCS_SEVERITY=9
 test -z "${INPUT_PHPCS_REPORT}" && INPUT_PHPCS_REPORT=checkstyle
 
 sh -c "/root/.composer/vendor/bin/phpcs --report=${INPUT_PHPCS_REPORT} --severity=${INPUT_PHPCS_SEVERITY} --standard=$INPUT_PHPCS_STANDARD $GITHUB_WORKSPACE -s $*"
